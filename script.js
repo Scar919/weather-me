9886baeba887296baa3ecc9875c9f7a152bdacac
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-09 12:31:11 -0700

    added to javascript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var apiKey = "9f067d1887d1d67972264e590d4c3197";
+
+var city ;
+var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+var cityArr = [];
 
 
 
